1762bd66dbe41f6e7077ab27608fdccfbf9e4e32
Carlos Zamora <czamora5@ucol.mx>
2022-09-10 22:20:05 -0500

    refactor: separate related functions into different files

---
+++ src/card.js
@@ -0,0 +1,56 @@
+import { cropText } from './util.js';
+
+export function generatePreviewCard(cardTemplate, picture, verbose = false) {
+  const card = document.importNode(cardTemplate, true);
+
+  setMediaElement(card, picture);
+
+  const cardTitle = card.querySelector('.card__title');
+  cardTitle.textContent = picture.title;
+
+  const cardDate = card.querySelector('.card__date');
+  cardDate.textContent = picture.date;
+  cardDate.setAttribute('datetime', picture.date);
+
+  const cardDesc = card.querySelector('.card__desc');
+  cardDesc.textContent = verbose ? picture.explanation : cropText(picture.explanation, 50);
+
+  if (verbose) {
+    card.querySelector('.card').classList.add('verbose');
+    setCopyright(card, picture);
+  } else {
+    const cardBtn = card.querySelector('.card__btn');
+    cardBtn.setAttribute('data-date', picture.date);
+  }
+
+  return card;
+}
+
+function setMediaElement(card, picture) {
+  const cardElement = card.querySelector('.card');
+  const img = card.querySelector('img');
+
+  if (picture['media_type'] == 'image') {
+    img.alt = `Nasa picture of the day ${picture.date}`;
+    img.src = picture.url;
+  } else {
+    const iframe = document.createElement('iframe');
+    iframe.classList.add('card__img');
+    iframe.src = picture.url;
+    cardElement.replaceChild(iframe, img);
+  }
+}
+
+function setCopyright(card, picture) {
+  const cardContent = card.querySelector('.card__content');
+  const cardBtn = cardContent.querySelector('.card__btn');
+  cardContent.removeChild(cardBtn);
+
+  if (picture.copyright) {
+    const span = document.createElement('span');
+    span.textContent = `Copyright: ${picture.copyright}`;
+    span.classList.add('card__copyright');
+
+    cardContent.appendChild(span);
+  }
+}
---
+++ src/display.js
@@ -0,0 +1,48 @@
+import { generatePreviewCard } from './card.js';
+// import { hideSpinner, showSpinner } from './spinner.js';
+
+export function clearMainContent() {
+  const main = document.querySelector('main');
+  main.innerHTML = '';
+}
+
+export function printPictures(data) {
+  setDisplayMode();
+
+  const cardTemplate = document.getElementById('card-template').content;
+  const gallery = document.getElementById('gallery');
+  const fragment = document.createDocumentFragment();
+
+  data.forEach(picture => {
+    const card = generatePreviewCard(cardTemplate, picture);
+    fragment.appendChild(card);
+  });
+  gallery.appendChild(fragment);
+}
+
+export function printPicture(picture) {
+  setDisplayMode(true);
+
+  const cardTemplate = document.getElementById('card-template').content;
+  const main = document.querySelector('main');
+
+  const card = generatePreviewCard(cardTemplate, picture, true);
+  main.appendChild(card);
+}
+
+function setDisplayMode(singleImage = false) {
+  clearMainContent();
+
+  if (!singleImage) {
+    const main = document.querySelector('main');
+    createGallery(main);
+  }
+}
+
+function createGallery(parent) {
+  const gallery = document.createElement('div');
+  gallery.classList.add('grid');
+  gallery.setAttribute('id', 'gallery');
+
+  parent.appendChild(gallery);
+}
