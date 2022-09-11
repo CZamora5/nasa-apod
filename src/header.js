0d84447b6ca805df8f2f33cb1a1a855509bfc780
Carlos Zamora <czamora5@ucol.mx>
2022-09-11 14:20:35 -0500

    feat: add function to retrieve search parameters and to validate them

--- src/header.js
+++ src/header.js
@@ -1,3 +1,78 @@
+import { formatNumber } from './util.js';
+
+export function getSearchParams() {
+  const filterBy = getSelectedOption('#filter-by');
+  const searchBy = getSelectedOption('#search-by');
+
+  if (searchBy === 'month') {
+    const month = getSelectedOption('#month');
+    const year = getSelectedOption('#year');
+
+    return { filterBy, searchBy, month, year };
+  }
+
+  const date = getSelectedOption('#date');
+  return { filterBy, searchBy, date };
+}
+
+export function validateSearchParams(params) {
+  const response = {
+    validParams: true,
+    errors: {}
+  };
+  let minDate;
+
+  if (params.searchBy === 'month') {
+    if (params.month === '') {
+      response.errors.monthMissing = 'Please enter a month';
+      response.validParams = false;
+    }
+
+    if (!params.year) {
+      response.errors.yearMissing = 'Please enter a year';
+      response.validParams = false;
+    } else if (parseInt(params.year) !== Number(params.year)) {
+      response.errors.invalidYear = 'Please enter a valid year';
+      response.validParams = false;
+    } else {
+      minDate = new Date('1995-06');
+
+      params.year = Number(params.year);
+      const date = new Date(params.year, params.month - 1);
+      response.query = `${formatNumber(params.year, 4)}-${formatNumber(params.month)}`;
+
+      if (date < minDate) {
+        response.errors.invalidYear = 'The month can not be before Jun 1995';
+        response.validParams = false;
+      }
+
+      if (date > new Date()) {
+        response.errors.invalidYear = 'The month can not be after the current month';
+        response.validParams = false;
+      }
+    }
+  } else {
+    minDate = new Date('1995-06-16');
+    const date = new Date(params.date);
+    
+    if (date < minDate) {
+      response.errors.invalidDate = 'The date can not be before Jun 16, 1995';
+      response.validParams = false;
+    }
+
+    if (date > new Date()) {
+      response.errors.invalidDate = 'The date can not be after the current date';
+      response.validParams = false;
+    }
+  }
+
+  return response;
+}
+
+export function displayErrorMessages() {
+  return;
+}
+
 (function activateHamburgerMenu(selector) {
   const hamburgerMenu = document.querySelector(selector);
 
@@ -24,9 +99,7 @@
   });
 })('#search-by', { month: '.navbar__options__search-inputs-month', day: '.navbar__options__search-inputs-date' });
 
-export function getFilterByOption(selector) {
+function getSelectedOption(selector) {
   const select = document.querySelector(selector);
   return select.value;
 }
-
-
