15b354bb27a721cc809ae04bef425e5afad6cb55
Carlos Zamora <czamora5@ucol.mx>
2022-09-17 04:42:50 -0500

    feat: implement setPage function which gives functionality to the pagination

--- src/pagination.js
+++ src/pagination.js
@@ -1,5 +1,61 @@
-function activatePaginationButtons() {
-  return;
+import { validateSearchParams } from './header.js';
+import { getPreviousDay, getNextDay, formatDate, getNextMonth, getPreviousMonth, getDateInStringFormat } from './util.js';
+import { searchImage, searchImages } from './index.js';
+
+export function setPage(current, lastSearchParams, previousBtnSelector, nextBtnSelector) {
+  console.log(current);
+  hidePagination();
+  showPagination()
+  const previousBtn = document.querySelector(previousBtnSelector);
+  const nextBtn = document.querySelector(nextBtnSelector);
+
+  if (lastSearchParams.searchBy === 'month') {
+    const previousMonth = getPreviousMonth(current);
+    const nextMonth = getNextMonth(current);
+    previousBtn.querySelector('span').textContent = getDateInStringFormat(previousMonth);
+    nextBtn.querySelector('span').textContent = getDateInStringFormat(nextMonth);
+
+    previousBtn.addEventListener('click', () => {
+      [lastSearchParams.year, lastSearchParams.month] = previousMonth.split('-');
+      const paramsDetails = validateSearchParams(lastSearchParams);
+      console.log(paramsDetails);
+      if (paramsDetails.validParams) {
+        searchImages(paramsDetails.query, lastSearchParams);
+      }
+    });
+
+    nextBtn.addEventListener('click', () => {
+      [lastSearchParams.year, lastSearchParams.month] = nextMonth.split('-');
+      const paramsDetails = validateSearchParams(lastSearchParams);
+      console.log(paramsDetails);
+      if (paramsDetails.validParams) {
+        searchImages(paramsDetails.query, lastSearchParams);
+      }
+    });
+  } else {
+    const previousDay = formatDate(getPreviousDay(current));
+    const nextDay = formatDate(getNextDay(current));
+    previousBtn.querySelector('span').textContent = getDateInStringFormat(previousDay);
+    nextBtn.querySelector('span').textContent = getDateInStringFormat(nextDay);
+
+    previousBtn.addEventListener('click', () => {
+      lastSearchParams.date = previousDay;
+      const paramsDetails = validateSearchParams(lastSearchParams);
+      console.log(paramsDetails);
+      if (paramsDetails.validParams) {
+        searchImage(lastSearchParams.date, lastSearchParams);
+      }
+    });
+
+    nextBtn.addEventListener('click', () => {
+      lastSearchParams.date = nextDay;
+      const paramsDetails = validateSearchParams(lastSearchParams);
+      console.log(paramsDetails);
+      if (paramsDetails.validParams) {
+        searchImage(lastSearchParams.date, lastSearchParams);
+      }
+    });
+  }
 }
 
 export function showPagination() {
